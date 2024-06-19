a209ca0d8405057b03bbbe968c429c4e3131c417
jhon03 <hoyosb90@gmail.com>
2024-06-18 22:57:25 -0500

    config bd para local de nuevo

--- src/config/db/mongoDB.db.js
+++ src/config/db/mongoDB.db.js
@@ -2,8 +2,9 @@ const mongoose = require('mongoose');
 
 const dbConecction = async() => { 
     try {
-        await mongoose.connect(process.env.MONGODB_CNN_LOCAL);  
+        await mongoose.connect(process.env.MONGODB_LOCAL);  
         console.log('Base de datos levantada en local'); 
+        
     } catch (error) {
         console.error("Error al levantar la base de datos:", error);
         throw error;
