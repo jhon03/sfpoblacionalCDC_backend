d4e4d2c28fecc0623652b6b33b1a31a39119ad82
jhon03 <hoyosb90@gmail.com>
2024-06-19 18:12:09 -0500

    configuración de base de datos en remoto

--- src/config/db/mongoDB.db.js
+++ src/config/db/mongoDB.db.js
@@ -2,9 +2,9 @@ const mongoose = require('mongoose');
 
 const dbConecction = async() => { 
     try {
-        await mongoose.connect(process.env.MONGODB_LOCAL);  
-        console.log('Base de datos levantada en local'); 
-        
+        await mongoose.connect(process.env.MONGODB_CNN);  
+        console.log('Base de datos levantada en remoto'); 
+        console.log(process.env.PORT)
     } catch (error) {
         console.error("Error al levantar la base de datos:", error);
         throw error;
